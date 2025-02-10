9a5cbc81190fc6242fb9916d3faa940b354e9a77
Juyoung8563 <zxcvbnm856311@gmail.com>
2025-02-10 16:44:04 +0900

    fix model

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ app.post("/", async (req, res) => {
   const TOGETHER_BASE_URL = "https://api.together.xyz";
   const GROQ_URL = "https://api.groq.com/openai/v1/chat/completions";
   const TURBO_MODEL = "meta-llama/Llama-3.3-70B-Instruct-Turbo-Free";
+  const GROQ_LLAMA_MODEL = "llama3-70b-8192";
   const FLUX_MODEL = "black-forest-labs/FLUX.1-schnell-Free";
   const MIXTRAL_MODEL = "mixtral-8x7b-32768";
   const DEEPSEEK_MODEL = "deepseek-ai/DeepSeek-R1-Distill-Llama-70B-free";
@@ -74,9 +75,12 @@ app.post("/", async (req, res) => {
   // 2-1. 이미지를 생성하는 프롬프트
   // llama-3-3-70b-free (together) -> 속도 측면
   const prompt = await callAI({
-    url: `${TOGETHER_BASE_URL}/v1/chat/completions`,
-    apiKey: TOGETHER_API_KEY,
-    model: TURBO_MODEL,
+    // url: `${TOGETHER_BASE_URL}/v1/chat/completions`,
+    // apiKey: TOGETHER_API_KEY,
+    // model: TURBO_MODEL,
+    url: GROQ_URL,
+    apiKey: GROQ_API_KEY,
+    model: GROQ_LLAMA_MODEL,
     // text,
     text: `${text}를 바탕으로 맛집 추천에 어울리는 AI 이미지 생성을 위한 200자 이내의 영어 프롬프트를 작성해줘`,
   }).then((res) => res.choices[0].message.content);
@@ -104,9 +108,12 @@ app.post("/", async (req, res) => {
   // 3-1. 설명을 생성하는 프롬프트
   // llama-3-3-70b-free (together)
   const prompt2 = await callAI({
-    url: `${TOGETHER_BASE_URL}/v1/chat/completions`,
-    apiKey: TOGETHER_API_KEY,
-    model: TURBO_MODEL,
+    // url: `${TOGETHER_BASE_URL}/v1/chat/completions`,
+    // apiKey: TOGETHER_API_KEY,
+    // model: TURBO_MODEL,
+    url: GROQ_URL,
+    apiKey: GROQ_API_KEY,
+    model: GROQ_LLAMA_MODEL,
     // text,
     text: `${text}를 바탕으로 맛집 추천에 어울리는 설명 생성을 위한 200자 이내의 한글 프롬프트를 작성해줘`,
   }).then((res) => res.choices[0].message.content);
